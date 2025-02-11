b874fe958ee7ff365aa4fa3b2c14c6b887ab1b30
Mohammad Alkhatib <mohammad.alkhatib@web.de>
2025-02-11 10:36:48 +0100

    Added simple comments to explain how things work.

--- core/src/main/java/me/nulldoubt/advancedtilemaps/AdvancedTileMaps.java
+++ core/src/main/java/me/nulldoubt/advancedtilemaps/AdvancedTileMaps.java
@@ -14,53 +14,121 @@ import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.*;
 
 public class AdvancedTileMaps extends ApplicationAdapter {
 
     private static final Vector2 temp = new Vector2();
 
+    // These here are the variables for our 'dirt' tile layer.
     private Texture dirt;
     private Texture dirtOverlay;
     private TileLayer dirtLayer;
 
+    // and these here are the variables for our 'grass' tile layer.
     private Texture grass;
     private Texture grassOverlay;
     private TileLayer grassLayer;
 
+    /*
+        and these here are the necessary variables
+        for us to draw the tile layers.
+    */
     private Batch batch;
     private ShaderProgram shader;
-    private FitViewport viewport;
+
+    /*
+
+        and finally, this is a viewport that's optional
+        but really recommended. It manages both our camera
+        and the gl-viewport state.
+
+        It allows us draw in our own world units and
+        provides better compatibility for other screen
+        resolutions.
+
+        We constructed a FitViewport, which fixes the
+        aspect ratio of your defined world coordinates
+        and causes black borders to appear to keep that
+        aspect ratio.
+
+        So, let's say you usually have a 16x16 tile and
+        in all of your draw-calls, you use
+
+            batch.draw(tile, x, y, 16f, 16f); -> tile is 16x16 pixels, depending on screen resolution.
+
+        that will hardly scale up if you build a nice
+        game, and it will be even harder if you let
+        your physics library use the pixels instead
+        of your actual world-bounds.
+
+        So, *my* recommended approach is to actually
+        create a fit-viewport with your desired world
+        bounds. The world bounds should have the same
+        aspect as your native resolution for the game.
+
+        Say, if your native resolution is 800x450, your
+        aspect ratio is 16:9 (e.g. 450 / 800 = 1.78 = 16:9)
+        and your fit-viewport should be of that aspect
+        ratio, I simply chose the size (16, 9) here.
+
+        Now, before rendering, you have to apply your
+        viewport, and you can use your world units
+        without ever worrying:
+
+            batch.draw(tile, x, y, 1f, 1f); -> tile is 1x1 world units, independent of screen resolution.
+
+     */
+    private Viewport viewport;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
+
+        // initializing the dirt tile layer members.
         dirt = new Texture("Dirt.png");
         dirtOverlay = new Texture("DirtOverlay.png");
+
+        // dirt tile layer is pixel art, we don't want it to be blurry.
         dirtOverlay.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
+
+        // URGENT: this here is essential for the overlay tile maps to work properly.
         dirtOverlay.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
 
+        // initializing the grass tile layer members.
         grass = new Texture("Grass.png");
         grassOverlay = new Texture("GrassOverlay.png");
+
+        // grass tile layer is pixel art as well, and we don't want it to be blurry either.
         grassOverlay.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest);
+
+        // URGENT: this here is essential for the overlay tile maps to work properly.
         grassOverlay.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
 
+        // creating our shader program.
         shader = new ShaderProgram(
             Gdx.files.internal("shaders/overlay.vert").readString(),
             Gdx.files.internal("shaders/overlay.frag").readString()
         );
+
+        // a post-compilation check to see if our shader program compiled successfully.
         if (!shader.isCompiled())
             throw new RuntimeException("Unable to compile shader: " + shader.getLog());
 
+        // creating our sprite-batch.
         batch = new SpriteBatch();
-        viewport = new FitViewport(16f, 9f);
 
+        // creating our viewport, with our world bounds being of the same aspect-ratio of our native resolution.
+        viewport = new StretchViewport(16f, 9f);
+
+        // initializing our dirt tile layer.
         dirtLayer = new TileLayer(new TextureRegion(dirt), dirtOverlay, shader, 64, 64, 16f, 16f, 1f / 16f);
-        dirtLayer.reset(true);
+        dirtLayer.reset(true); // we don't want our dirt layer to have holes!
 
+        // initializing our grass tile layer.
         grassLayer = new TileLayer(new TextureRegion(grass), grassOverlay, shader, 64, 64, 16f, 16f, 1f / 16f);
-        grassLayer.reset(false);
+        grassLayer.reset(false); // our grass layer should be empty at first.
 
+        // setting the default input processor.
         Gdx.input.setInputProcessor(new InputAdapter() {
 
             @Override
@@ -102,35 +170,54 @@ public class AdvancedTileMaps extends ApplicationAdapter {
 
     @Override
     public void render() {
-        ScreenUtils.clear(Color.BLACK);
+        ScreenUtils.clear(Color.BLACK); // clear the screen.
 
         final OrthographicCamera camera = (OrthographicCamera) viewport.getCamera();
         final float width = (viewport.getWorldWidth() / 2f);
         final float height = (viewport.getWorldHeight() / 2f);
 
+        // here, we clamp the camera position to our world boundaries.
         camera.position.set(
             MathUtils.clamp(camera.position.x, width + 1f, dirtLayer.tilesX * dirtLayer.tileWidth * dirtLayer.unitScale - width),
             MathUtils.clamp(camera.position.y, height + 1f, dirtLayer.tilesY * dirtLayer.tileHeight * dirtLayer.unitScale - height),
             0f
         );
+
+        /*
+            URGENT: if you have a viewport or configured the tile
+            layers to use a unit-scale, then apply your viewport here!
+         */
         viewport.apply();
 
+        // setting the viewport camera projection matrix.
         batch.setProjectionMatrix(camera.combined);
-        batch.begin();
+        batch.begin(); // begin the batch.
 
-        dirtLayer.render(batch);
-        grassLayer.render(batch);
+        /*
+            rendering the layers, warning: the order of
+            rendering plays a huge role, and it's up to
+            you to manage it properly.
+         */
+        dirtLayer.render(batch); // first comes the dirt.
+        grassLayer.render(batch); // then comes the grass.
 
-        batch.end();
+        batch.end(); // end the batch.
     }
 
     @Override
     public void resize(final int width, final int height) {
+        /*
+            when the window resizes, we'd like our viewport
+            to update the internal screen size to properly
+            handle the conversion of our world units.
+         */
         viewport.update(width, height, true);
     }
 
     @Override
     public void dispose() {
+
+        // and here, we dispose everything!
 
         dirt.dispose();
         dirtOverlay.dispose();
@@ -140,6 +227,7 @@ public class AdvancedTileMaps extends ApplicationAdapter {
 
         shader.dispose();
         batch.dispose();
+
     }
 
 }
